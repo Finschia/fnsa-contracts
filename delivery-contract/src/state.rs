f3f566c3362235f93f96a8e7f490beefe92ef06d
Daisuke Iuchi <42408108+da1suk8@users.noreply.github.com>
2023-06-06 11:39:34 +0900

    fix: add description of Subcontractor

--- delivery-contract/src/state.rs
+++ delivery-contract/src/state.rs
@@ -1,6 +1,9 @@
 use cosmwasm_std::Addr;
 use serde::{Deserialize, Serialize};
 
+/// The `item_id` starts with `1` and `0` is defined as the initial value
+///
+///  `addr` is address of subcontractor and `""` is defined as the initial value
 #[derive(Serialize, Deserialize)]
 pub struct Subcontractor {
     pub item_id: u32,
